8cc11a0766a30d2e088cdd357282afee931e9942
Teddy-Li <litianyi01@163.com>
2018-05-26 20:41:58 +0800

    Add files via upload
    目前几点想到的相关事情：
    1，ShootGame 的state在pause里面会被用到
    2，可不可以把ShootGame 106-112行的代码包装成一个gameover之后重置游戏状态的函数reInit()这样重置游戏状态之后就可以重新调出ShootGame界面（调用在Over.java第31行）
    3，目前鼠标点击之后字体的标记颜色保存在Start里面，看一下要不要挪一下
    4，选项界面目前还没有任何选项放在里面

---
+++ ShootingGame/src/shootgame/GameObject.java
@@ -0,0 +1,98 @@
+
+package shootgame;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * 最主要的游戏物体类，所有游戏内物体都会继承自此类
+ * 编写新的GameObject，主要靠继承其update()，render()和onCollision()方法
+ *
+ * @author hehao
+ */
+public abstract class GameObject {
+    public ShootGame game;         // 存储指向这个物体所属游戏类的指针
+    public boolean enabled = true; // 表示这个物体是否被设置为有效，如果无效则会在下一帧被删除
+    protected double x; // x坐标，采用double为了计算速度和碰撞更加精确，下同
+    protected double y; // y坐标
+    protected int width;
+    protected int height;
+    protected BufferedImage image;   //图片
+    protected PhysicsEngine.Collider collider = null; // 是否允许碰撞检测，允许则在子类中设置collider
+
+    GameObject(ShootGame game) {
+        this.game = game;
+    }
+
+    /**
+     * 在游戏主循环里，每一帧对每个游戏物体调用此方法，来更新游戏的状态
+     * 因此对于新的游戏物体要表现不同行为的话，只需重载此方法即可
+     */
+    public void update() {
+
+    }
+
+    /**
+     * 对游戏的画布的paint()方法会对每一个游戏物体调用此函数
+     * 这个函数默认会画上一张图片
+     * 如果要绘制更多特效的话请重载此函数
+     *
+     * @param g 用来绘图的Graphics对象
+     */
+    public void render(Graphics g) {
+        g.drawImage(image, getX(), getY(), null);
+    }
+
+    /**
+     * 物理引擎在检测到两个物体存在碰撞后，会调用这两个物体的onCollision函数
+     * 重载这个函数如果你希望两个物体碰撞会发生一些游戏事件
+     *
+     * @param other
+     */
+    public void onCollision(GameObject other) {
+        // 故意留空
+    }
+
+    public boolean isEnabled() { return enabled; }
+
+    public int getX() {  
+        return (int)x;
+    }  
+  
+    public void setX(int x) {  
+        this.x = x;  
+    }  
+  
+    public int getY() {  
+        return (int)y;
+    }  
+  
+    public void setY(int y) {  
+        this.y = y;
+    }  
+  
+    public int getWidth() {  
+        return width;  
+    }  
+  
+    public void setWidth(int width) {  
+        this.width = width;  
+    }  
+  
+    public int getHeight() {  
+        return height;  
+    }  
+  
+    public void setHeight(int height) {  
+        this.height = height;  
+    }  
+  
+    public BufferedImage getImage() {  
+        return image;  
+    }  
+  
+    public void setImage(BufferedImage image) {  
+        this.image = image;  
+    }
+  
+}
---
+++ ShootingGame/src/shootgame/ShootGame.java
@@ -0,0 +1,321 @@
+package shootgame;
+
+import java.awt.Font;  
+import java.awt.Color;  
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.*;
+import java.lang.*;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 这是整个游戏的核心类
+ *
+ * @author qinyuxuan, hehao
+ */
+public class ShootGame extends JPanel {
+    /**
+     * 窗体的大小
+     */
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 800;
+
+    /**
+     * 游戏的当前状态: START RUNNING PAUSE GAME_OVER
+     */
+    private static final int START = 0;  
+    private static final int RUNNING = 1;  
+    private static final int PAUSE = 2;  
+    private static final int GAME_OVER = 3;
+
+    // 资源图片
+    public static BufferedImage background;
+    public static BufferedImage start;
+    public static BufferedImage pause;
+    public static BufferedImage gameover;
+
+    static {
+        background = ResourceManager.getImage("background1");
+        start = ResourceManager.getImage("start");
+        pause = ResourceManager.getImage("pause");
+        gameover = ResourceManager.getImage("gameover");
+    }
+
+    private int state;
+    public int score = 0; // 得分
+
+    private Timer timer; // 定时器
+    private int interval = 10; // 时间间隔(毫秒)
+    public long lastUpdatedTime;
+    public long currentTime;
+    public long deltaTime;
+
+    // 管理敌人的生成，道具的生成等操作
+    public SceneManager sceneManager = new SceneManager(this);
+
+    // 负责处理用户的输入
+    public InputManager inputManager = new InputManager(this);
+
+    // 负责处理物理
+    public PhysicsEngine physicsEngine = new PhysicsEngine(this);
+
+    public Projectile[] projectiles = new Projectile[1000]; // 子弹数组
+    public int projectilesLastEmptyPosition; //记录数组留空的位置
+    public Enemy[] enemies = new Enemy[100]; // 敌人数组
+    public int enemiesLastEmptyPosition; //记录敌人数组留空的位置
+    public Player player = new Player(this, 0); // 玩家
+
+    public static void main(String[] args) {
+    	GameFrame frame = new GameFrame();
+        frame.setVisible(true); // 尽快调用paint
+    }
+
+    /** 启动执行代码 */
+    public void start() {
+
+        // 鼠标监听事件
+        MouseAdapter l = new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) { // 鼠标进入
+                if (state == PAUSE) { // 暂停状态下运行
+                    state = RUNNING;
+                    ShootGame.this.requestFocus();
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) { // 鼠标退出
+                if (state == RUNNING) { // 游戏未结束，则设置其为暂停
+                    state = PAUSE;
+                }
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) { // 鼠标点击
+                switch (state) {
+                    case START:
+                        state = RUNNING; // 启动状态下运行
+                        ShootGame.this.requestFocus();
+                        break;
+                    case GAME_OVER: // 游戏结束，清理现场
+                        enemies = new Enemy[0]; // 清空飞行物
+                        projectiles = new Projectile[0]; // 清空子弹
+                        player = new Player(ShootGame.this, 0); // 重新创建英雄机
+                        score = 0; // 清空成绩
+                        state = START; // 状态设置为启动
+                        break;
+                }
+            }
+        };
+
+        this.addMouseListener(l); // 处理鼠标点击操作
+        this.addMouseMotionListener(l); // 处理鼠标滑动操作
+
+        timer = new Timer(); // 主流程控制
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // 更新时序信息
+                lastUpdatedTime = currentTime;
+                currentTime = new Date().getTime();
+                deltaTime = currentTime - lastUpdatedTime;
+
+                if (state == RUNNING) { // 运行状态
+                    // 先刷新敌人
+                    sceneManager.update();
+
+                    // 依次更新投射物，敌人和玩家的状态
+                    for (Projectile proj : projectiles)
+                        if (proj != null) proj.update();
+                    for (Enemy enemy : enemies)
+                        if (enemy != null) enemy.update();
+                    player.update();
+
+                    physicsEngine.detectCollision(); // 物理引擎检测碰撞
+
+                    garbageCollection(); // 删除越界飞行物，死亡飞行物及子弹
+
+                    checkGameOver(); // 检查游戏结束
+                }
+                repaint(); // 重绘，调用paint()方法
+            }
+
+        }, interval, interval);
+    }
+
+    /** 画 */  
+    @Override  
+    public void paint(Graphics g) {  
+    	//是否需要调用super.paint()?
+        g.drawImage(background, 0, 0, null); // 画背景图
+
+        // 依次绘制投射物，敌人和玩家
+        for (Projectile proj : projectiles) {
+            if (proj != null && proj.enabled) proj.render(g);
+        }
+        for (Enemy enemy : enemies) {
+            if (enemy != null && enemy.enabled) enemy.render(g);
+        }
+        player.render(g);
+
+        paintScore(g); // 画分数  
+        paintState(g); // 画游戏状态  
+    }  
+
+    /** 画分数 */  
+    private void paintScore(Graphics g) {
+        int x = 10; // x坐标  
+        int y = 25; // y坐标  
+        Font font = new Font(Font.SANS_SERIF, Font.BOLD, 22); // 字体  
+        g.setColor(new Color(0xFF0000));  
+        g.setFont(font); // 设置字体  
+        g.drawString("SCORE:" + score, x, y); // 画分数  
+        y=y+20; // y坐标增20  
+        g.drawString("LIFE:" + player.getLife(), x, y); // 画命
+    }  
+  
+    /** 画游戏状态 */  
+    private void paintState(Graphics g) {
+        switch (state) {  
+        case START: // 启动状态  
+            g.drawImage(start, 0, 0, null);  
+            break;  
+        case PAUSE: // 暂停状态  
+            g.drawImage(pause, 0, 0, null);  
+            break;  
+        case GAME_OVER: // 游戏终止状态  
+            g.drawImage(gameover, 0, 0, null);  
+            break;  
+        }  
+    }
+
+    /**
+     * 在场景中增加一些子弹，但是可能出现数组满的罕见情况，此时返回false
+     * @param newProjectiles 要增加的子弹数组
+     * @return 如果添加成功返回true, 否则false
+     */
+    public boolean addProjectiles(Projectile[] newProjectiles) {
+        // 对每个子弹，寻找一个数组空位加进去
+        int successfullyAdded = 0;
+
+        OuterLoop:
+        for (Projectile projectile : newProjectiles) {
+            for (int j = projectilesLastEmptyPosition, cnt = 0;
+                 cnt < projectiles.length;
+                 j = (j + 1) % projectiles.length, cnt++) {
+                if (projectiles[j] == null) {
+                    projectiles[j] = projectile;
+                    projectilesLastEmptyPosition = j;
+                    successfullyAdded++;
+                    continue OuterLoop;
+                }
+            }
+        }
+
+        return successfullyAdded == newProjectiles.length;
+    }
+
+    /**
+     * 在场景中增加一些敌人，但是可能出现数组满的罕见情况，此时返回false
+     * @param newEnemies 要增加的敌人数组
+     * @return 如果添加成功返回true, 否则false
+     */
+    public boolean addEnemies(Enemy[] newEnemies) {
+        // 对每个新敌人，寻找一个数组空位加进去
+        int successfullyAdded = 0;
+
+        OuterLoop:
+        for (Enemy newEnemy : newEnemies) {
+            for (int j = enemiesLastEmptyPosition, cnt = 0;
+                 cnt < enemies.length;
+                 j = (j + 1) % enemies.length, cnt++) {
+                if (enemies[j] == null) {
+                    enemies[j] = newEnemy;
+                    enemiesLastEmptyPosition = j;
+                    successfullyAdded++;
+                    continue OuterLoop;
+                }
+            }
+        }
+
+        return successfullyAdded == newEnemies.length;
+    }
+
+    /**
+     * 返回游戏内所有物体
+     * @return 游戏内所有物体的数组
+     */
+    public GameObject[] getGameObjects() {
+        GameObject[] objects = new GameObject[enemies.length + projectiles.length + 1];
+        int i = 0;
+        for (Enemy enemy : enemies) {
+            if (enemy != null && enemy.enabled) {
+                objects[i++] = enemy;
+            }
+        }
+        for (Projectile projectile : projectiles) {
+            if (projectile != null && projectile.enabled) {
+                objects[i++] = projectile;
+            }
+        }
+        objects[i++] = player;
+
+        GameObject[] result = new GameObject[i];
+        for (int j = 0; j < i; j++) {
+            result[j] = objects[j];
+        }
+        return result;
+    }
+
+    /** 删除越界飞行物，死亡飞行物及子弹 */
+    private void garbageCollection() {
+
+        for (int i = 0; i < projectiles.length; i++) {
+            if (projectiles[i] == null) continue;
+            if (!projectiles[i].enabled || outOfBounds(projectiles[i])) {
+                projectiles[i] = null;
+                projectilesLastEmptyPosition = i;
+            }
+        }
+
+        for (int i = 0; i < enemies.length; i++) {
+            if (enemies[i] == null) continue;
+            if (!enemies[i].enabled || outOfBounds((enemies[i]))) {
+                enemies[i] = null;
+                enemiesLastEmptyPosition = i;
+            }
+        }
+    }
+
+    private boolean outOfBounds(GameObject object) {
+        return (object.getX() < -object.getWidth()
+                || object.getX() > WIDTH + object.getWidth()
+                || object.getY() < -object.getHeight()
+                || object.getY() > HEIGHT + object.getHeight());
+    }
+  
+    /** 检查游戏结束 */
+    private void checkGameOver() {
+        if (isGameOver()) {
+            state = GAME_OVER; // 改变状态
+        }
+    }
+
+    private boolean isGameOver() {
+        return player.getLife() <= 0;
+    }
+
+    private void printInfo() {
+        for (Projectile projectile : projectiles) {
+            if (projectile == null) continue;
+            System.out.println(projectile.getX() + " " + projectile.getY());
+        }
+    }
+
+}
